2e63248d7f24317744a0984a1e7be6c134c32317
SilviaApadet <sylviaapadet01@gmail.com>
2025-04-14 14:11:48 +0300

    updated

--- Backend/app/routes/admin.py
+++ Backend/app/routes/admin.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # app/admin.py
 
 from flask import Blueprint
@@ -8,6 +7,3 @@ admin_bp = Blueprint('admin', __name__)
 @admin_bp.route('/admin')
 def admin_home():
     return "Welcome to the admin section"
-=======
-from app.admin import admin_bp
->>>>>>> c93418bef0aaff8d36d835125b69d9755f45b9ad
